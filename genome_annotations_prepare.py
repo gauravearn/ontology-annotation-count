59c764f7f2125c0d602f503c55b1fbb0eb3bbef4
Gaurav Sablok <sablokg@gmail.com>
2023-06-29 17:09:43 +0530

    Create genome_annotations_prepare.py

---
+++ genome_annotations_prepare.py
@@ -0,0 +1,46 @@
+import arguably
+@arguably.command
+def prepareGeneEnrichment(file = False, count= False, annotation = False):
+    """
+    _summary_
+    A parser to prepare the files for the functional
+    enrichment of the gene categories across the 
+    functionally annotated microbiome
+    Arguments:
+        file -- _description_
+        a text file containing your gene ontologies
+    """
+    if file:
+        go_content = []
+        go_summarize = []
+        with open("/Users/gauravsablok/Desktop/CHPC/Test_Datasets_CHPC/test.txt", 'r') as file:
+            for line in file.readlines():
+                file_go.append(line.strip())
+            for i in range(len(file_go)):
+                go_final.append(file_go[i].replace(";", ""))
+    if file and count:
+           go_content = []
+           go_summarize = []
+           go_count = []
+           with open("/Users/gauravsablok/Desktop/CHPC/Test_Datasets_CHPC/test.txt", 'r') as file:
+            for line in file.readlines():
+                file_go.append(line.strip())
+            for i in range(len(file_go)):
+                go_final.append(file_go[i].replace(";", "")) 
+                count.append([{i:gene_summarize.count(i)} for i in set(go_summarize)])
+                return gene_ontologies, count, unique
+    if annotation:
+        while True:
+            take_annotation = input("Please enter the path for the \
+                               annotation file")
+            take_annotation_col = input("Please enter the annotation columns")
+            annotations = pd.read_csv("take_annotation", sep = ",")
+            annotations_col = annotations["take_annotation_col"].dropna().tolist()
+            final_annotations = [row for col in ([i.split(";") for i in go]) for row in col]
+            final_annotations_count = [{i:final_annotations.count(i)} for i in set(final_annotations)]
+            annotations_name = [i for i in set(final_annotations)]
+            annotations_count = [final_annotations.count(i) for i in set(final_annotations_count)]
+            annotations_dataframe = pd.DataFrame([(i,j)for i,j in zip(annotations_count, annotations_name)])
+        return final_annotations, final_annotations_count, annotations_name, annotations_dataframe   
+    if __name__ == "__main__":
+    arguably.run()         
